e8835aa79eeb7c83d90d1ece78588e791c3a9d50
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-12 13:37:44 +0200

    Added Login and Logout logic

--- auth/register/views.py
+++ auth/register/views.py
@@ -1,6 +1,7 @@
 
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate, logout
 
 from auth.register.serializers import UserSerializer
 
@@ -11,4 +12,22 @@ class RegisterViewSet(viewsets.ViewSet):
             serializer.save()
             return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+class LoginViewSet(viewsets.ViewSet):
+    def create(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            return Response({"message":"Login successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"Invalid credentials111"}, status=status.HTTP_400_BAD_REQUEST)
         
+
+class LogoutViewSet(viewsets.ViewSet):
+    def create(self, request):
+        logout(request)
+        return Response({"message":"Logout successful"}, status=status.HTTP_200_OK)
--- videoflix/urls.py
+++ videoflix/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from auth.register.views import RegisterViewSet
+from auth.register.views import LoginViewSet, LogoutViewSet, RegisterViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     path('register/', RegisterViewSet.as_view({'post': 'create'}), name='RegisterView'),
+    path('login/', LoginViewSet.as_view({'post': 'create'}), name='LoginView'),
+    path('logout/', LogoutViewSet.as_view({'post': 'create'}), name='LogoutView'),
 ]
