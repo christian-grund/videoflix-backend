ed72aa34b95fef5415d0feda0307b3766043632b
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-13 16:53:29 +0200

    Redis config

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -65,6 +65,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
+CACHES = {    
+    "default": {        
+        "BACKEND": "django_redis.cache.RedisCache",        
+        "LOCATION": "redis://127.0.0.1:6379/1",        
+        "OPTIONS": {            
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"        
+            },        
+            "KEY_PREFIX": "videoflix"    
+            }}
+
 ROOT_URLCONF = 'videoflix.urls'
 
 TEMPLATES = [
