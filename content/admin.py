5ba124b33402384f187c37e10450acc7701ba8e9
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-17 16:19:27 +0200

    Set up PostgreSQL

--- content/admin.py
+++ content/admin.py
@@ -7,7 +7,7 @@ from import_export.admin import ImportExportModelAdmin
 class VideoItemResource(resources.ModelResource):
 
     class Meta:
-        model = VideoItem  # or 'core.Book'
+        model = VideoItem  
 
 @admin.register(VideoItem)
 class VideoItemAdmin(ImportExportModelAdmin):
--- videoflix/settings.py
+++ videoflix/settings.py
@@ -133,10 +133,21 @@ WSGI_APPLICATION = 'videoflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videoflix_database',
+        'USER': 'chris',
+        'PASSWORD': '1234', 
+        'HOST': 'localhost', 
+        'PORT': '',
     }
 }
 
