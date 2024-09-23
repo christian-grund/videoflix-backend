f63e9f42f4dde4f3694a6e7dabe92c1077ba2f40
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-23 11:56:50 +0200

    After uploading a video, a screenshot for the thumbnail is created

--- content/signals.py
+++ content/signals.py
@@ -1,10 +1,11 @@
-from content.tasks import convert_video
-
 from .models import VideoItem
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import django_rq
+
+from content.tasks import convert_video, create_video_screenshot
 import os
+import subprocess
 
 
 # sender: von welcher Instanz gesendet wurde
@@ -18,9 +19,13 @@ def video_post_save(sender, instance, created, **kwargs):
     if created:
         print('New video created')
         queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue(convert_video, instance.video_file.path, '_360p', 'scale=640:360')
-        queue.enqueue(convert_video, instance.video_file.path, '_720p', 'scale=1280:720')
-        queue.enqueue(convert_video, instance.video_file.path, '_1080p', 'scale=1920:1080')
+        # queue.enqueue(convert_video, instance.video_file.path, '_360p', 'scale=640:360')
+        # queue.enqueue(convert_video, instance.video_file.path, '_720p', 'scale=1280:720')
+        # queue.enqueue(convert_video, instance.video_file.path, '_1080p', 'scale=1920:1080')
+        # Pfad für den Screenshot definieren
+        video_file_name_without_extension = os.path.splitext(os.path.basename(instance.video_file.name))[0]
+        screenshot_path = os.path.join(os.path.dirname(instance.video_file.path), f'{video_file_name_without_extension}.jpg')        
+        queue.enqueue(create_video_screenshot, instance.video_file.path, screenshot_path)
         queue.enqueue(delete_original_video, instance.video_file.path)
         
 
@@ -34,12 +39,13 @@ def video_post_delete(sender, instance, **kwargs):
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
 
+
 def delete_original_video(video_path):
     if os.path.isfile(video_path):
         os.remove(video_path)
         print(f'Originalvideo {video_path} wurde gelöscht.')
 
-        
+
 # pre: davor, post: danach
 # pre_delete und post_delete auch möglich
 # diese Art der Registrierung ist veraltet
--- content/tasks.py
+++ content/tasks.py
@@ -8,27 +8,30 @@ def convert_video(source, resolution, scale):
     cmd = '{} -i "{}" -vf {} -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(ffmpeg_path, source, scale, target)
     subprocess.run(cmd, shell=True)
 
-def convert_360p(source):
-    ffmpeg_path = '/Users/christian/usr/ffmpeg/ffmpeg'  # Vollständiger Pfad
-    base, ext = os.path.splitext(source)
-    target = base + '_360p' + ext
-    cmd = '{} -i "{}" -vf "scale=640:360" -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(ffmpeg_path, source, target)
-    subprocess.run(cmd, shell=True)
 
+def create_video_screenshot(video_path, output_image_path, time="00:00:05"):
+    """
+    Erstellt einen Screenshot eines Videos mit ffmpeg.
+    """
+    # time.sleep(5)  # 10 Sekunden warten
+    if not os.path.exists(video_path):
+        print(f"Video-Datei nicht gefunden: {video_path}")
+        return
 
-def convert_720p(source):
-    ffmpeg_path = '/Users/christian/usr/ffmpeg/ffmpeg'  # Vollständiger Pfad
-    base, ext = os.path.splitext(source)
-    target = base + '_720p' + ext
-    cmd = '{} -i "{}" -vf "scale=1280:720" -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(ffmpeg_path, source, target)
-    subprocess.run(cmd, shell=True)
+    # Kurze Verzögerung, um sicherzustellen, dass die Datei verfügbar ist
+    
 
-
-def convert_1080p(source):
-    ffmpeg_path = '/Users/christian/usr/ffmpeg/ffmpeg'  # Vollständiger Pfad
-    base, ext = os.path.splitext(source)
-    target = base + '_1080p' + ext
-    cmd = '{} -i "{}" -vf "scale=1920:1080" -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(ffmpeg_path, source, target)
-    subprocess.run(cmd, shell=True)
-
+    try:
+        command = [
+            'ffmpeg',
+            '-ss', time,
+            '-i', video_path,
+            '-vframes', '1',
+            '-q:v', '2',
+            output_image_path
+        ]
+        subprocess.run(command, check=True)
+        print(f'Screenshot erstellt: {output_image_path}')
+    except subprocess.CalledProcessError as e:
+        print(f'Fehler bei der Screenshot-Erstellung: {e}')
 
