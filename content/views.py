0d31fc6c51e319fd765dfc945661cfda1ba9da6c
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-09-27 10:57:29 +0200

    On videopage, user sees his own videos and the ones for everyone

--- content/views.py
+++ content/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.db import models
+from django.db.models import Q
 from content.admin import VideoItemResource
 from content.models import VideoItem
 from content.serializers import VideoItemSerializer
 from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 import logging
@@ -12,33 +14,28 @@ import logging
 logger = logging.getLogger(__name__)
 
 class VideoItemViewSet(viewsets.ModelViewSet):
-    print('VideoItemViewSet')
     queryset = VideoItem.objects.all()
     serializer_class = VideoItemSerializer
-    permission_classes = [IsAuthenticated]  # Nur authentifizierte Benutzer haben Zugriff
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Authentifizierung nicht erforderlich für lesende Zugriffe
 
     def get_queryset(self):
-        # Überprüfe, ob der Benutzer authentifiziert ist
         user = self.request.user
-        logger.info(f"Anfrage von Benutzer: {user} (Authenticated: {user.is_authenticated})")
-        
-        if user.is_authenticated:
-            logger.info(f"Benutzer ID: {user.id}, Benutzername: {user.username}")
-            return VideoItem.objects.filter(user=user)
-        
-        logger.warning("Benutzer nicht authentifiziert")
-        return VideoItem.objects.none()  # Leere Abfrage, wenn kein Benutzer authentifiziert ist
 
+        if user.is_authenticated:
+            # Benutzer bekommt seine eigenen Videos und die öffentlichen (user=None) Videos angezeigt
+            return VideoItem.objects.filter(models.Q(user=user) | models.Q(user__isnull=True))
+
+        # Nicht authentifizierte Benutzer sehen nur öffentliche Videos (user=None)
+        return VideoItem.objects.filter(user__isnull=True)
+    
     def list(self, request, *args, **kwargs):
-        # Liste der Videos des angemeldeten Benutzers zurückgeben
         queryset = self.get_queryset()
         if not queryset.exists():
-            logger.info(f"Keine Videos gefunden für Benutzer {self.request.user.username}")
-            return Response({'detail': 'No videos found for this user.'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'detail': 'No videos found.'}, status=status.HTTP_404_NOT_FOUND)
         
         serializer = self.get_serializer(queryset, many=True)
-        logger.info(f"Videos gefunden: {serializer.data}")
         return Response(serializer.data)
+
 
 
 def export_videoitems_json(request):
