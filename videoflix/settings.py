bb96a274cdecae34b41f64ebeda3220afa874e14
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-10-24 14:52:32 +0200

    Added DRF settings

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -220,6 +220,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',  # Ensure JSON responses
+    ),
 }
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
