6567634f98415f4d35b388dcf1653f7533bb9145
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-10-25 16:10:50 +0200

    Updated media_url

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -128,8 +128,8 @@ TEMPLATES = [
 ]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/var/www/videoflix-backend/media/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/videoflix-backend/media'
 
 WSGI_APPLICATION = 'videoflix.wsgi.application'
 
