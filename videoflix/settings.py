f2ed922b0bbadbfdb5af3182a6708510fc70b6fc
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-10-25 16:18:25 +0200

    Added Logging Test

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -9,6 +9,8 @@ from pathlib import Path
 from decouple import config
 import ssl
 import certifi
+import logging
+logging.error("Test error log - if you see this, logging is working")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
