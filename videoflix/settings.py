003ce1916608bf46e40ef5889f55381db2bccdd5
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-11-07 11:31:37 +0100

    Changed corsmiddleware placement

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -61,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'videoflix.middlewares.AddAcceptRangesHeaderMiddleware',
 ]
