98b9f7cf02bdf3314442821b8f5d7c0bd314f0bf
christian-grund <christian.grund@outlook.de>
2024-11-08 09:20:11 +0100

    Updated allowed_hosts

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -26,11 +26,11 @@ SECRET_KEY = 'django-insecure-^b-ugi2=(7k2x2g9oiw2m9*#*vlv%uobcpu@o9$))m8ma@4k03
 DEBUG = True
 
 ALLOWED_HOSTS = [  
+    'videoflix-backend.christian-grund.dev',
     'http://localhost:4200/',
     'localhost',
     '127.0.0.1',
     '35.246.138.52',
-    'videoflix-backend.christian-grund.dev'
     ]
 
 
