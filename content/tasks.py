9b85c738d98312b915d6730355734768dfb7504d
christian-grund <christian.grund@outlook.de>
2024-11-09 10:57:57 +0100

    Updated tasks.py

--- content/tasks.py
+++ content/tasks.py
@@ -13,8 +13,8 @@ def convert_video(source, resolution, scale):
     if not os.path.exists(source):
         raise FileNotFoundError(f"Source file '{source}' does not exist.")
     
-    ffmpeg_path = '/Users/christian/usr/ffmpeg/ffmpeg'  
-    # ffmpeg_path = '/usr/bin/ffmpeg'
+    # ffmpeg_path = '/Users/christian/usr/ffmpeg/ffmpeg'  
+    ffmpeg_path = '/usr/bin/ffmpeg'
     base, ext = os.path.splitext(os.path.basename(source))
     target_base = f"{base}{resolution}"
     
