1240ecd6f21d5a5a7e2a41bfcbaf45a37174a501
christian-grund <christian.grund@outlook.de>
2024-12-09 10:56:01 +0100

    Added new fields for user detail view in admin.py

--- content/admin.py
+++ content/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from content.models import VideoItem
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_active')
 
 
 class VideoItemResource(resources.ModelResource):
@@ -18,3 +25,4 @@ class VideoItemAdmin(ImportExportModelAdmin):
     Admin interface for managing VideoItem objects with import/export capabilities.
     """
     list_display = ['title']
+
